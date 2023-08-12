bd73fc8330fd031d532a515931b1d27ea5e81519
Shrinivas Sesadri <shrinivassesadri@Shrinivass-MacBook-Pro.local>
2023-08-12 18:43:20 +0530

    first commit

---
+++ ask.py
@@ -0,0 +1,46 @@
+import sys
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+embeddings = OpenAIEmbeddings()
+from langchain.chains import RetrievalQA
+
+#to read pdf 
+doc_reader = PdfReader(sys.argv[2])
+
+raw_text = ''
+for i, page in enumerate(doc_reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+
+# Splitting up the text into smaller chunks for indexing
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 1000,
+    chunk_overlap  = 200, #striding over the text
+    length_function = len,
+)
+texts = text_splitter.split_text(raw_text)
+
+# Download embeddings from OpenAI
+
+docsearch = FAISS.from_texts(texts, embeddings)
+
+# set up FAISS as a generic retriever
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":4})
+
+# create the chain to answer questions
+rqa = RetrievalQA.from_chain_type(llm=OpenAI(),
+                                  chain_type="stuff",
+                                  retriever=retriever,
+                                  return_source_documents=True)
+
+# 
+query = sys.argv[1]
+print(rqa(query)['result'])
---
+++ index.js
@@ -0,0 +1,82 @@
+const { Configuration, OpenAIApi } = require('openai');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const multer = require('multer'); // For handling file uploads
+const app = express();
+const { PythonShell } = require('python-shell');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
+const port = 3080;
+
+
+
+const configuration = new Configuration({
+    organization: "org-4RxO9OW9Pb09NWZOYU2oiFTk",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+// Set up multer to handle file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Specify the directory to store uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.listen(port, () => {
+    console.log(`Example app listening at https://localhost:${port}`)
+})
+
+
+
+
+app.post('/',async (req, res) => {
+    const { message } = req.body;
+    console.log("input:",message);
+    let options = {
+      scriptPath: "/Users/shrinivassesadri/Drive/DocAsk/",
+      args: [`${message}`,p],
+    };
+    const pyShell = new PythonShell('ask.py', options);
+    
+    // Listen for Python script output
+    pyShell.on('message', (messages) => {
+      // const response = await (message);
+      res.json({
+        message: messages,
+      });
+    });
+    
+    // End the PythonShell process
+    pyShell.end((err) => {
+      if (err) {
+        console.error('Error ending PythonShell:', err);
+      }
+    });
+    
+    // console.log(response.data.choices[0].text)
+    
+});
+// const response = await openai.createCompletion({
+    //     model: "text-davinci-003",
+    //     prompt: "Say this is a test",
+    //     max_tokens: 7,
+    //     temperature: 0,
+
+
+// Handle the PDF upload
+app.post('/upload-pdf', upload.single('pdfFile'), (req, res) => {
+  console.log('PDF uploaded:', req.file);
+  p = req.file.path;
+  res.status(200).send('PDF uploaded successfully.');
+});
+
